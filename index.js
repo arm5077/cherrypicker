5c2fa26e82a62bb170ab8edceb67639320acba27
Andrew McGill <arm5077@gmail.com>
2015-03-27 23:05:41 -0400

    Adds /trees endpoint

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var pg = require('pg');
+
+// Turn on server
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("We're live at port " + port + ".");
+});
+
+// Endpoint to deliver information about a tree
+app.get("/trees/:id", function(request, response){
+	
+	pg.connect("postgres://amcgill@localhost/trees", function(err, client, done) {
+		if( err )
+			throw err;
+		client.query("SELECT * FROM trees.trees WHERE id = $1", [request.params.id], function(err, result){
+			if( err )
+				throw err;
+
+			response.status(200).json(result.rows);
+
+			done();
+			client.end();
+		});
+
+	});
+});
+
+// Endpoint to get nearest cherry tree
+app.get("/nearest/:lat/:lng", function(request, response){
+	console.log(request.params.lng);
+})
+
+
+
+//SELECT *, ST_DISTANCE(geom, ST_SetSRID(ST_MakePoint(-77.054505, 38.935440), 4326)) as distance FROM trees.trees WHERE common_name LIKE '%cherry%' ORDER BY distance ASC LIMIT 5 
