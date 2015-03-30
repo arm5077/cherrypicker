2f2b87b904a93abcb0f4d35c162d517fd26ff4e7
Andrew McGill <arm5077@gmail.com>
2015-03-30 13:32:56 -0400

    Add 'nearest' endpoint logic

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.listen(port, function(){
 app.get("/trees/:id", function(request, response){
 	
 	pg.connect("postgres://amcgill@localhost/trees", function(err, client, done) {
-		if( err )
-			throw err;
+		if( err ) throw err;
 		client.query("SELECT * FROM trees.trees WHERE id = $1", [request.params.id], function(err, result){
 			if( err )
 				throw err;
@@ -29,8 +28,21 @@ app.get("/trees/:id", function(request, response){
 });
 
 // Endpoint to get nearest cherry tree
-app.get("/nearest/:lat/:lng", function(request, response){
+app.get("/nearest/:lng/:lat", function(request, response){
 	console.log(request.params.lng);
+	
+	pg.connect("postgres://amcgill@localhost/trees", function(err, client, done){
+		if( err ) throw err;	
+		client.query("SELECT *, ST_DISTANCE(geom, ST_SetSRID(ST_MakePoint($1, $2), 4326)) as distance \
+			FROM trees.trees \
+			WHERE common_name LIKE '% cherry%' \
+			ORDER BY distance ASC LIMIT 5 ", 
+		[request.params.lng, request.params.lat], function(err, result){
+			if(err) throw err;
+			response.status(200).json(result.rows);
+		});
+	});
+	
 })
 
 
