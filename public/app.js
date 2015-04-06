3646a6426cb3022a0d2d17221633ab6ba9854819
Andrew McGill <arm5077@gmail.com>
2015-04-06 17:52:33 -0400

    Add accuracy check

--- public/app.js
+++ public/app.js
@@ -8,6 +8,7 @@ app.controller("treeController", ["$scope", "$http", "$sce", function($scope, $h
 	
 	$scope.flipped = false;
 	$scope.onStage = "search";
+	$scope.accuracy = "";
 	$scope.console = console;
 
 	$scope.renderHTML = function(html){
@@ -20,32 +21,36 @@ app.controller("treeController", ["$scope", "$http", "$sce", function($scope, $h
 		if( navigator.geolocation){
 			navigator.geolocation.watchPosition(function(position){
 				console.log(position);
-				$http.get("nearest/" + position.coords.longitude + "/" + position.coords.latitude)
-					.success(function(data, status, headers, config){
-						console.log("got response");
-						$scope.loaded = true;
-						$scope.result = data[0];
-						$scope.result.distance = getDistance(data[0].x, data[0].y, position.coords.longitude, position.coords.latitude) + " " + getDirection(data[0].x, data[0].y, -122.6819,45.5200);
-						$scope.flipped = false;
-						
-						var request = {
-							origin: position.coords.latitude + "," + position.coords.longitude,
-							destination: data[0].y + "," + data[0].x,
-							travelMode: google.maps.TravelMode.WALKING
-						}
-						
-						directionsService.route(request, function(result, status) {
-							if (status == google.maps.DirectionsStatus.OK) {
-								$scope.route = result.routes[0];
-								console.log($scope.route);
-							} else {
-								console.log(status)
+				if(position.coords.accuracy < $scope.accuracy || $scope.accuracy = ""){
+					$scope.accuracy = position.coords.accuracy;
+					
+					$http.get("nearest/" + position.coords.longitude + "/" + position.coords.latitude)
+						.success(function(data, status, headers, config){
+							console.log("got response");
+							$scope.loaded = true;
+							$scope.result = data[0];
+							$scope.result.distance = getDistance(data[0].x, data[0].y, position.coords.longitude, position.coords.latitude) + " " + getDirection(data[0].x, data[0].y, -122.6819,45.5200);
+							$scope.flipped = false;
+
+							var request = {
+								origin: position.coords.latitude + "," + position.coords.longitude,
+								destination: data[0].y + "," + data[0].x,
+								travelMode: google.maps.TravelMode.WALKING
 							}
-			  			});
-					})
-					.error(function(err){
-						console.log(err);
-					});
+
+							directionsService.route(request, function(result, status) {
+								if (status == google.maps.DirectionsStatus.OK) {
+									$scope.route = result.routes[0];
+									console.log($scope.route);
+								} else {
+									console.log(status)
+								}
+				  			});
+						})
+						.error(function(err){
+							console.log(err);
+						});
+				}
 			}, function(error){
 				console.log(error);
 			});
