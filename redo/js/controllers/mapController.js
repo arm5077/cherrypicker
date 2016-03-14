cc9ba19f55f4ee0ffcc0240079f0ddad15d97c17
Andrew McGill <arm5077@gmail.com>
2016-03-14 15:33:54 -0400

    Fix more bugs

--- redo/js/controllers/mapController.js
+++ redo/js/controllers/mapController.js
@@ -1,50 +1,41 @@
 angular.module("app")
 .controller("mapController", ["$scope", "$http", "$sce", function($scope, $http, $sce){	
-console.log("bazinga");
 
-	if( !$scope.position.latitude )
+	if( $scope.$parent.trees.length <= 0 )
 		window.location.hash = "loading";
+	else {
+		var map = null;
+		map = new google.maps.Map(document.getElementById('mapContainer'), {
+			center: {lat: $scope.position.latitude, lng: $scope.position.longitude},
+			zoom: 12
+		});
 
-	var map = new google.maps.Map(document.getElementById('mapContainer'), {
-		center: {lat: $scope.position.latitude, lng: $scope.position.longitude},
-		zoom: 12
-	});
-	
-	var bounds = new google.maps.LatLngBounds();
-	$scope.$parent.trees.forEach(function(tree){
+		var bounds = new google.maps.LatLngBounds();
+		$scope.$parent.trees.forEach(function(tree){
+			var marker = new google.maps.Marker({
+				position: {lat: tree.y, lng: tree.x},
+				labelClass: "treeFont",
+				map: map,
+				icon: '/assets/tree_marker.png'
+			});
+			bounds.extend(marker.getPosition())
+
+			marker.addListener('click', function() {
+				window.location.hash = "/tree/" + tree.id;
+			});
+
+		});
+
 		var marker = new google.maps.Marker({
-			position: {lat: tree.y, lng: tree.x},
+			position: {lat: $scope.position.latitude, lng: $scope.position.longitude},
 			labelClass: "treeFont",
 			map: map,
-			icon: '/assets/tree_marker.png'
+			icon: '/assets/house_marker.png'
 		});
-		bounds.extend(marker.getPosition())
-		
-		marker.addListener('click', function() {
-			window.location.hash = "/tree/" + tree.id;
-		});
-		
-	});
-	
-	var marker = new google.maps.Marker({
-		position: {lat: $scope.position.latitude, lng: $scope.position.longitude},
-		labelClass: "treeFont",
-		map: map,
-		icon: '/assets/house_marker.png'
-	});
-	
-
-
-	map.fitBounds(bounds);
-	
 
 
 
-	
-	
-	
-	
-
-	
+		map.fitBounds(bounds);
+	}
 
 }]);
